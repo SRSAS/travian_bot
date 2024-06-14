2e5758e608d513b499932cb8ab92a5abb5626593
Sebastiao Sousa <sebastiao.sousa@tecnico.ulisboa.pt>
2024-06-14 13:46:52 +0100

    refactor: change selenium_manager import alias

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from utils import *
-import selenium_manager as sm
+import selenium_manager as SM
 
 
 @log_execution_time
 def main():
-    selenium_manager = sm.SeleniumManager()
-    international_5_farming_menu = Menu(title=f"{sm.INTERNATIONAL_5}: farming", options=[Option(
+    selenium_manager = SM.SeleniumManager()
+    international_5_farming_menu = Menu(title=f"{SM.INTERNATIONAL_5}: farming", options=[Option(
         name="Select undefended oases", command=as_command(selenium_manager.select_undefended_oases_farms,
-                                                           server=sm.INTERNATIONAL_5))])
+                                                           server=SM.INTERNATIONAL_5))])
 
-    international_5_menu = Menu(title=f"{sm.INTERNATIONAL_5}", options=[Option(name="farming",
+    international_5_menu = Menu(title=f"{SM.INTERNATIONAL_5}", options=[Option(name="farming",
                                                                                command=international_5_farming_menu)]
                                 )
     main_menu = Menu(title="Server selection", options=[Option(name=international_5_menu.title,
                                                                command=international_5_menu)],
-                     on_close=[as_command(selenium_manager.close_driver, server=sm.INTERNATIONAL_5)])
+                     on_close=[as_command(selenium_manager.close_driver, server=SM.INTERNATIONAL_5)])
 
     main_menu()
 
